31de437c334b08a4871168c77ce24d5b1a2c84a7
Ritesh Dhamale <ritesh.dhamale17@gmail.com>
2026-01-24 09:31:41 +0530

    New Changes

--- Dart/Example.dart
+++ Dart/Example.dart
@@ -1,3 +1,26 @@
-void main() {
-  print('Hello, Dart!');
+int factorial(int n) {
+  if(n<=0){
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+//fibonacci series
+
+List<int> fibonacci(int n) {
+  // Your code here
+  List<int> fib = [0, 1];
+  for (int i = 2; i < n; i++) {
+    fib.add(fib[i - 1] + fib[i - 2]);
+  }
+  return fib.sublist(0, n);
+}
+
+void main(){
+  int number = 5;
+print("Factorial of $number is ${factorial(number)}");
+
+//fibonacci
+print("6. Fibonacci (first 10): ${fibonacci(10)}");
 }
